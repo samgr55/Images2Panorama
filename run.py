601d24b75a347cc7cd5a1dafe98e35cc08142b8b
Samer Rahmeh <samgr93@gmail.com>
2023-05-23 00:54:16 +0300

    UPDATED

--- run.py
+++ run.py
@@ -34,35 +34,67 @@ if (video):
     sam = list(map(path.__add__,  myList))
     width = int(img.shape[1])
     height = int(img.shape[0])
+    if(width > 3000 or height > 3000):
+        scalar = 0.3
+    else:
+        scalar = 0.7
     if (width > height):
         print("[INFO]: Video has been taken in Landscape Mode")
         stitcher = stitching.Stitcher()     # taken from CV2 library
         settings = {"detector": "sift", "confidence_threshold": 0.2}
         stitcher = stitching.Stitcher(**settings)
+        dim = (720,1280)
+        images = []
+        tempCount = 0
+        tempPath = "cache/"
+        for samsom in sam:
+           image = cv2.imread(samsom)
+           resized = cv2.resize(image,(int(image.shape[1]*scalar),int(image.shape[0]*scalar)),interpolation = cv2.INTER_AREA)
+           cv2.imwrite(tempPath+str(tempCount)+".png", resized)
+           tempCount +=1
+        myList2 = os.listdir(tempPath)
+        sam = list(map(tempPath.__add__,  myList2))
         panorama = stitcher.stitch(sam)
-        status = True
+        
     else:
         print("[INFO]: Video has been taken in Portrait Mode")
         stitcher2 = cv2.Stitcher_create()
         images = []
         for samsom in sam:
            image = cv2.imread(samsom)
-           images.append(image)
-        (status,panorama) = stitcher2.stitch(sam)
+           resized = cv2.resize(image,(int(image.shape[1]*scalar),int(image.shape[0]*scalar)),interpolation = cv2.INTER_AREA)
+           images.append(resized)
+        (status,panorama) = stitcher2.stitch(images)
         
 
 else:
-    path = "imgs_2/"
+    path = "imgs_1/"
     myList = os.listdir(path)
     img = cv2.imread(path+myList[0])
     width = int(img.shape[1])
     height = int(img.shape[0])
+    if(width > 3000 or height > 3000):
+        scalar = 0.3
+    else:
+        scalar = 0.7
     if (width > height):
         print("[INFO]: Photos have been taken in Landscape Mode")
+
         stitcher = stitching.Stitcher()     # taken from CV2 library
         settings = {"detector": "sift", "confidence_threshold": 0.2}
         stitcher = stitching.Stitcher(**settings)
         sam = list(map(path.__add__,  myList))
+        dim = (1280,720)
+        images = []
+        tempCount = 0
+        tempPath = "cache/"
+        for samsom in sam:
+           image = cv2.imread(samsom)
+           resized = cv2.resize(image,(int(image.shape[1]*scalar),int(image.shape[0]*scalar)),interpolation = cv2.INTER_AREA)
+           cv2.imwrite(tempPath+str(tempCount)+".png", resized)
+           tempCount +=1
+        myList2 = os.listdir(tempPath)
+        sam = list(map(tempPath.__add__,  myList2))
         panorama = stitcher.stitch(sam)
         status = True
         
@@ -70,14 +102,19 @@ else:
        print("[INFO]: Photos have been taken in Portrait Mode")
        stitcher2 = cv2.Stitcher_create()
        sam = list(map(path.__add__,  myList))
+       dim = (1080,2340)
        images = []
        for samsom in sam:
            image = cv2.imread(samsom)
-           images.append(image)
+           resized = cv2.resize(image,(int(image.shape[1]*scalar),int(image.shape[0]*scalar)),interpolation = cv2.INTER_AREA)
+           
+           images.append(resized)
        (status,panorama) = stitcher2.stitch(images)
        
 
-
+cv2.imshow('image',resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 if (status):
     print("[INFO]: The panoramic photo has been created successfully") # this is what you want Omar right? if yes, take the status value
 else:
